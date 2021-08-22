75e9858ea634d73addbde684b5d3a63d560de38d
Paul Pranta <70230389+Pranta0@users.noreply.github.com>
2021-08-23 00:00:19 +0600

    Add files via upload

---
+++ regresspy/regresspy/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'regresspy',
+    version = '1.1',
+    packages = ['regresspy.Regression'],
+    package_dir = {'' : 'regresspy'},
+    url = 'http://example.com',
+    liscense = 'MIT',
+    author = "Paul Pranta",
+    author_email = 'prantapaul727@gmail.com',
+    description = 'Regression using Gradient Descent from scratch'
+)
